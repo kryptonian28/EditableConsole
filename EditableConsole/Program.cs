6d21d9c572eab1ba372f41489d9bbfcd30f2460d
kryptonian28 <kryptonian28@outlook.com>
2024-02-11 16:31:44 +0530

    Add project files.

---
+++ EditableConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EditableConsole
+{
+    internal class Program
+    {
+        private string s;
+
+        public string Name
+        {
+            get { return s; }
+            set { s = value; }
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Program program = new Program();
+            program.Name = "testName";
+            Console.WriteLine(program.Name);
+            Console.WriteLine("Completed!");
+
+        }
+    }
+}
