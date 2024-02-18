22d5edc6348f282b6b7ca6ddb39939c1ac66f6d2
kryptonian28 <kryptonian28@outlook.com>
2024-02-19 01:52:32 +0530

    unformatted

--- EditableConsole/Program.cs
+++ EditableConsole/Program.cs
@@ -1,10 +1,12 @@
-﻿
+
+using System.Diagnostics.CodeAnalysis;
+
 namespace EditableConsole
 {
     internal class Program
     {
         private string s;
-        private string a;
+        private string b;
 
         public string Name
         {
@@ -19,31 +21,40 @@ namespace EditableConsole
             program.Name = "testName";
 
             a = 10;
+            a = 10 + 10;
+            string sql = "Select * from table where id>10";
+            string s = "Select * from names where names like '%A%'";
+            Console.Write(sql);
         }
+   /// <summary>Adds 2 numbers.</summary>
+        /// <param name="p">First number</param>
+        /// <param name="q">Second number</param>
 
-        public void abc(int p, int q)
+   public void bcd(int p, int q)
         {
-            int a = 5;
-
+            Console.WriteLine( p+q);
         }
+        /// <summary>This is a summary.</summary>
 
-        public void abc()
-        {
+        public void bcd()
+   {
 
         }
         /// <summary>
         /// 
         /// </summary>
         /// <param name="p"></param>
-        public void abc(int p)
+        public void bcd(int p)
         {
 
-        }     
-        
+        }
+        /// <summary>This is a summary.</summary>
+        /// <param name="a">This is a placeholder description for a.</param>
 
-        public void abc(string a)
+        public void bcd(string a)
         {
 
         }
     }
 }
+//Hello, GitHub!17-02-2024 00:48:38
