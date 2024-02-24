ddaa536b2fa8c3788e7300cad3fe9e85827a06fe
Kryptonian28 <singhgaurav28@outlook.com>
2024-02-24 19:03:20 +0530

    Commit operation config

--- EditableConsole/Program.cs
+++ EditableConsole/Program.cs
@@ -5,27 +5,27 @@ namespace EditableConsole
 {
     internal class Program
     {
-        private string s;
-        private string b;
+        private string DummyCompletion0;
+        private string DummyCompletion36;
 
         public string Name
         {
-            get { return s; } 
-            set { s = value; }
+            get { return DummyCompletion0; } 
+            set { DummyCompletion0 = value; }
         }
 
         static void Main(string[] args)
         {
-            int a = 0;
+            int DummyCompletion64023 = 0;
             Program program = new Program(); 
             program.Name = "testName"; 
 
-            a = 10; 
-            a = 10;  
-            a = 10 + 10;
-            string sql = "Select * from table where id>10";
-            string s = "Select * from names where names like '%A%'"; 
-            Console.Write(sql);
+            DummyCompletion64023 = 10; 
+            DummyCompletion64023 = 10;  
+            DummyCompletion64023 = 10 + 10;
+            string DummyCompletion13673 = "Select * from table where id>10";
+            string DummyCompletion36790 = "Select * from names where names like '%A%'"; 
+            Console.Write(DummyCompletion13673);
         }
    /// <summary>Adds 2 numbers.</summary>
         /// <param name="p">First number</param>
@@ -35,7 +35,7 @@ namespace EditableConsole
         {
             Console.WriteLine( p+q);
         }
-        /// <summary>This is a summary.</summary>
+        /// <summary>This is DummyCompletion64023 summary.</summary>
 
         public void bcd()
    {
@@ -49,8 +49,8 @@ namespace EditableConsole
         {
 
         }
-        /// <summary>This is a summary.</summary>
-        /// <param name="a">This is a placeholder description for a.</param>
+        /// <summary>This is DummyCompletion64023 summary.</summary>
+        /// <param name="a">This is DummyCompletion64023 placeholder description for DummyCompletion64023.</param>
 
         public void bcd(string a)
         {
